8379e5175a4247f1f1bcbc8643161a2d765ed11a
lab-ubuntu <pengfeigaothu@gmail.com>
2017-05-09 13:44:25 +0800

    ecg config

---
+++ ecg/run_docker.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -p 0.0.0.0:80:80 -v /home/alex/LabGit/ECG_random_walk/experiments/record_test/results/P-images:/usr/share/nginx/html -v nginx_example.conf:/etc/nginx/nginx.conf:ro --name nginx nginx
